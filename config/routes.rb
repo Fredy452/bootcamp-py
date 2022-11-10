aacd98cf4462b3e05bc50a7d30ef1979b3e9dcce
Fredy Arce <fredyarce452@gmail.com>
2022-11-09 21:26:47 -0300

    Agregando gestor de menu funcional

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
-  resources :navs
+
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
 
   mount Decidim::Core::Engine => '/'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
+# Agreagamos las rutas de nav a las rutas de decidim Admin
+Decidim::Admin::Engine.routes.draw do
+  constraints(->(request) { Decidim::Admin::OrganizationDashboardConstraint.new(request).matches? }) do
+    resources :navs
+  end
 end
